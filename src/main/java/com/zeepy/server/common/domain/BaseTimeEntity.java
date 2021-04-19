87312a3bd4af9177d0317cc0f069ee54ca841055
KimGyeong <rundeshot@kau.kr>
2021-04-20 00:49:47 +0900

    feat: Review 도메인 클래스 구현

---
+++ src/main/java/com/zeepy/server/common/domain/BaseTimeEntity.java
@@ -0,0 +1,19 @@
+package com.zeepy.server.common.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import lombok.Getter;
+
+/**
+ * Created by KimGyeong 4/19/20.
+ */
+@Getter
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+	@CreatedDate
+	private LocalDateTime createdDate;
+}
