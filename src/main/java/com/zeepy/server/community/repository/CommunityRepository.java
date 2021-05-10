aa5f6b32d197dd9fe5bc20dd8368cf6fbf025f29
hyunjong_96 <ahajongs@gmail.com>
2021-05-10 13:23:18 +0900

    refactor : @Repository 추가
    -각 repository에 @Repository추가했습니다
    
    resolve : #3

--- src/main/java/com/zeepy/server/community/repository/CommunityRepository.java
+++ src/main/java/com/zeepy/server/community/repository/CommunityRepository.java
@@ -2,6 +2,8 @@ package com.zeepy.server.community.repository;
 
 import com.zeepy.server.community.domain.Community;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommunityRepository extends JpaRepository<Community,Long> {
 }
