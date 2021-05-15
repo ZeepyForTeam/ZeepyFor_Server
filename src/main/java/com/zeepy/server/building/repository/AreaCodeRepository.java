287d0beeae5affa59b4a3cf3894ddf11d7b39363
koreaaerospaceU <zx811@naver.com>
2021-05-15 18:54:40 +0900

    feat : building repository 생성

---
+++ src/main/java/com/zeepy/server/building/repository/AreaCodeRepository.java
@@ -0,0 +1,10 @@
+package com.zeepy.server.building.repository;
+
+import com.zeepy.server.building.domain.AreaCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Minky on 2021-05-15
+ */
+public interface AreaCodeRepository extends JpaRepository<AreaCode, Long> {
+}
---
+++ src/main/java/com/zeepy/server/building/repository/BuildingRepository.java
@@ -0,0 +1,10 @@
+package com.zeepy.server.building.repository;
+
+import com.zeepy.server.building.domain.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Minky on 2021-05-15
+ */
+public interface BuildingRepository extends JpaRepository<Building, Long> {
+}
