ec93c7a8b704d2ed066b6f6d815fcecc79902250
koreaaerospaceU <zx811@naver.com>
2021-05-15 18:57:41 +0900

    feat : 임시 Servcie 생성

---
+++ src/main/java/com/zeepy/server/building/service/BuildingDealService.java
@@ -0,0 +1,11 @@
+package com.zeepy.server.building.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Minky on 2021-05-15
+ */
+
+@Service
+public class BuildingDealService {
+}
