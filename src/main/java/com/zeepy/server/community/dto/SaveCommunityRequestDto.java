945c4a198d69f016d9a8be9d466b6ebcf7b5e483
hyunjong_96 <ahajongs@gmail.com>
2021-05-15 22:20:07 +0900

    refactor : 작곰 ReviewDto 피드백 반영
    - 줄바꿈
    - 접근제어자 명시
    
    resolve : #3

--- src/main/java/com/zeepy/server/community/dto/SaveCommunityRequestDto.java
+++ src/main/java/com/zeepy/server/community/dto/SaveCommunityRequestDto.java
@@ -32,6 +32,7 @@ public class SaveCommunityRequestDto {
 
     @NotEmpty(message = "제목은 필수입니다.")
     private String title;
+
     @NotEmpty(message = "내용은 필수입니다.")
     private String content;
 
--- src/main/java/com/zeepy/server/review/dto/ReviewDto.java
+++ src/main/java/com/zeepy/server/review/dto/ReviewDto.java
@@ -11,21 +11,21 @@ import java.util.List;
 @Setter
 @Getter
 public class ReviewDto {
-    Long user; // 변경 될 겁니다.
-    String address;
-    CommuncationTendency communcationTendency;
-    LessorGender lessorGender;
-    LessorAge lessorAge;
-    String lessorReview;
-    RoomCount roomCount;
-    MultiChoiceReview soundInsulation;
-    MultiChoiceReview pest;
-    MultiChoiceReview lightning;
-    MultiChoiceReview waterPressure;
-    List<Furniture> furnitures;
-    String review;
-    TotalEvaluation totalEvaluation;
-    List<String> imageUrls;
+    private Long user; // 변경 될 겁니다.
+    private String address;
+    private CommuncationTendency communcationTendency;
+    private LessorGender lessorGender;
+    private LessorAge lessorAge;
+    private String lessorReview;
+    private RoomCount roomCount;
+    private MultiChoiceReview soundInsulation;
+    private MultiChoiceReview pest;
+    private MultiChoiceReview lightning;
+    private MultiChoiceReview waterPressure;
+    private List<Furniture> furnitures;
+    private String review;
+    private TotalEvaluation totalEvaluation;
+    private List<String> imageUrls;
 
     public ReviewDto(
             Long user, String address,
