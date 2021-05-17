9507a626471ac3651e038c5795229f4a341908e2
hyunjong_96 <ahajongs@gmail.com>
2021-05-18 00:10:16 +0900

    feat : Community 참여하기 API RepositoryTest
    - repositoryTest
    
    resolve : #3

--- src/test/java/com/zeepy/server/community/domain/CommunityRepositoryTest.java
+++ src/test/java/com/zeepy/server/community/domain/CommunityRepositoryTest.java
@@ -1,13 +1,16 @@
 package com.zeepy.server.community.domain;
 
+import com.zeepy.server.community.dto.ParticipationDto;
 import com.zeepy.server.community.repository.CommunityLikeRepository;
 import com.zeepy.server.community.repository.CommunityRepository;
+import com.zeepy.server.community.repository.ParticipationRepository;
+import com.zeepy.server.user.domain.User;
+import com.zeepy.server.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
@@ -23,10 +26,15 @@ public class CommunityRepositoryTest {
     CommunityRepository communityRepository;
     @Autowired
     CommunityLikeRepository communityLikeRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ParticipationRepository participationRepository;
+
 
     @DisplayName("커뮤니티 저장하기 테스트")
     @Test
-    public void saveCommunity(){
+    public void saveCommunity() {
         //given
         communityRepository.save(Community.builder()
                 .communityCategory(CommunityCategory.FREESHARING)
@@ -47,5 +55,61 @@ public class CommunityRepositoryTest {
         Community communities = result.get(0);
         assertThat(communities.getTitle()).isEqualTo("asadasda");
         assertThat(communities.getProductName()).isEqualTo("무료나눔물건");
+    }
+
+    @DisplayName("참여하기 저장 테스트")
+    @Test
+    public void joinCommunity() {
+        //given
+        User user = userRepository.save(User.builder().name("작성자").build());
+        Community saveCommunity = communityRepository.save(neighborhoodfriend(user));
+        User joinUser = userRepository.save(User.builder().name("참여자").build());
+
+        //when
+        ParticipationDto participationDto = new ParticipationDto(saveCommunity,joinUser);
+        Participation participation = participationRepository.save(participationDto.toEntity());
+
+        //then
+        assertThat(participation.getCommunity().getId()).isEqualTo(saveCommunity.getId());
+        assertThat(participation.getUser().getId()).isEqualTo(joinUser.getId());
+        assertThat(participation.getUser().getName()).isEqualTo("참여자");
+    }
+
+    public Community jointpurchaseEntity(User user){
+        return Community.builder()
+                .communityCategory(CommunityCategory.JOINTPURCHASE)
+                .productName("공동구매물건")
+                .productPrice(10000)
+                .sharingMethod("만나서")
+                .targetNumberOfPeople(2)
+                .targetAmount(10000)
+                .user(user)
+                .title("같이 살사람")
+                .content("구해요")
+                .imageUrls(Arrays.asList("1", "2", "3"))
+                .build();
+    }
+
+    public Community freesharingEntity(User user){
+        return Community.builder()
+                .communityCategory(CommunityCategory.FREESHARING)
+                .productName("공동구매물건")
+                .sharingMethod("만나서")
+                .user(user)
+                .title("같이 살사람")
+                .content("구해요")
+                .imageUrls(Arrays.asList("1", "2", "3"))
+                .build();
+    }
+
+    public Community neighborhoodfriend(User user){
+        return Community.builder()
+                .communityCategory(CommunityCategory.NEIGHBORHOODFRIEND)
+                .user(user)
+                .targetAmount(10000)
+                .title("같이 살사람")
+                .content("구해요")
+                .imageUrls(Arrays.asList("1", "2", "3"))
+                .build();
     }
 }
