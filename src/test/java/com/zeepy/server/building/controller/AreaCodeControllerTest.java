7f8ba43f482afe1e72a7a917b0a53248d06329b1
koreaaerospaceU <zx811@naver.com>
2021-05-20 01:21:06 +0900

    feat : AreaCode Controller Test 제작 - AreaCode Controller Test 제작

---
+++ src/test/java/com/zeepy/server/building/controller/AreaCodeControllerTest.java
@@ -0,0 +1,62 @@
+package com.zeepy.server.building.controller;
+
+import com.zeepy.server.building.dto.AreaCodeRequestDto;
+import com.zeepy.server.building.service.AreaCodeService;
+import com.zeepy.server.common.ControllerTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+
+/**
+ * Created by Minky on 2021-05-20
+ */
+@DisplayName("AreaCode Controller Test")
+@WebMvcTest(AreaCodeController.class)
+class AreaCodeControllerTest extends ControllerTest {
+
+    @MockBean
+    private AreaCodeService areaCodeService;
+
+    private AreaCodeRequestDto makeAreaCodeRequestDto() {
+        return new AreaCodeRequestDto(
+                1L,
+                "test"
+        );
+    }
+
+    @Override
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext) {
+        super.setUp(webApplicationContext);
+    }
+
+    @Test
+    @DisplayName("CREATE AreaCode Test")
+    void uploadAreaCode() throws Exception {
+        AreaCodeRequestDto areaCodeRequestDto = makeAreaCodeRequestDto();
+        given(areaCodeService.create(any(AreaCodeRequestDto.class))).willReturn(1L);
+        doPost("/api/code", areaCodeRequestDto);
+    }
+
+    @Test
+    @DisplayName("UPDATE AreaCode Test")
+    void updateAreaCode() throws Exception {
+        AreaCodeRequestDto areaCodeRequestDto = makeAreaCodeRequestDto();
+        doNothing().when(areaCodeService).update(1L, areaCodeRequestDto);
+        doPut("/api/code/1", areaCodeRequestDto);
+    }
+
+    @Test
+    @DisplayName("DELETE AreaCode Test")
+    void deleteAreaCode() throws Exception {
+        doNothing().when(areaCodeService).deleteById(1L);
+        doDelete("/api/code/1");
+    }
+}
