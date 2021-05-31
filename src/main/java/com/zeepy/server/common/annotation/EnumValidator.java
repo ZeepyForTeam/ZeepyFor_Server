455d4338658732e851abc0ff3f34e9d9b9233290
KimGyeong <rundeshot@kau.kr>
2021-06-01 01:18:14 +0900

    refactor: EnumValidator 유효성 검사 로직 수정
    - 분기문 로직 수정으로 depth 1 감소

--- src/main/java/com/zeepy/server/common/annotation/EnumValidator.java
+++ src/main/java/com/zeepy/server/common/annotation/EnumValidator.java
@@ -15,13 +15,16 @@ public class EnumValidator implements ConstraintValidator<Enum, String> {
 	public boolean isValid(String value, ConstraintValidatorContext context) {
 		boolean result = false;
 		Object[] enumValues = this.annotation.enumClass().getEnumConstants();
-		if (enumValues != null) {
-			for (Object enumValue : enumValues) {
-				if (value.equals(enumValue.toString()) || (this.annotation.ignoreCase() && value.equalsIgnoreCase(
-					enumValue.toString()))) {
-					result = true;
-					break;
-				}
+
+		if (enumValues == null) {
+			return result;
+		}
+
+		for (Object enumValue : enumValues) {
+			if (value.equals(enumValue.toString()) || (this.annotation.ignoreCase() && value.equalsIgnoreCase(
+				enumValue.toString()))) {
+				result = true;
+				break;
 			}
 		}
 		return result;
