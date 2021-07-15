c72d073ada573bf2935927191ecd967f66fc57f1
koreaaerospaceU <zx811@naver.com>
2021-07-15 22:25:53 +0900

    fix: review test 중복 제거 - 리뷰 테스트 중복 생성 제거 - Building 종속 repository 어노테이션 추가

--- src/main/java/com/zeepy/server/building/repository/BuildingDealRepository.java
+++ src/main/java/com/zeepy/server/building/repository/BuildingDealRepository.java
@@ -2,6 +2,7 @@ package com.zeepy.server.building.repository;
 
 import com.zeepy.server.building.domain.BuildingDeal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.Optional;
 /**
  * Created by Minky on 2021-05-15
  */
+@Repository
 public interface BuildingDealRepository extends JpaRepository<BuildingDeal, Long> {
     Optional<BuildingDeal> findByFloorAndBuilding_Id(int floor, Long id);
 }
--- src/main/java/com/zeepy/server/building/repository/BuildingRepository.java
+++ src/main/java/com/zeepy/server/building/repository/BuildingRepository.java
@@ -4,6 +4,7 @@ import com.zeepy.server.building.domain.Building;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,7 @@ import java.util.Optional;
 /**
  * Created by Minky on 2021-05-15
  */
+@Repository
 public interface BuildingRepository extends JpaRepository<Building, Long> {
     Optional<Building> findByAddressContaining(String address);
 
