ab6f5a92547a630b84e69f2064e936c18063cf67
KimGyeong <rundeshot@kau.kr>
2021-07-20 01:41:26 +0900

    refactor: BuildingLikeService 중복 코드 제거 - getBuildingLikeById 메소드 추출

--- src/main/java/com/zeepy/server/building/service/BuildingLikeService.java
+++ src/main/java/com/zeepy/server/building/service/BuildingLikeService.java
@@ -1,5 +1,10 @@
 package com.zeepy.server.building.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.zeepy.server.building.domain.Building;
 import com.zeepy.server.building.domain.BuildingLike;
 import com.zeepy.server.building.dto.BuildingLikeRequestDto;
@@ -7,12 +12,9 @@ import com.zeepy.server.building.dto.BuildingLikeResponseDto;
 import com.zeepy.server.building.repository.BuildingLikeRepository;
 import com.zeepy.server.building.repository.BuildingRepository;
 import com.zeepy.server.common.CustomExceptionHandler.CustomException.NoContentException;
+
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 /**
  * Created by Minky on 2021-06-02
@@ -28,12 +30,14 @@ public class BuildingLikeService {
     @Transactional
     public Long create(BuildingLikeRequestDto buildingLikeRequestDto) {
         Building building = buildingRepository
-                .findById(buildingLikeRequestDto.getBuildingId())
-                .orElseThrow(NoContentException::new);
+            .findById(buildingLikeRequestDto.getBuildingId())
+            .orElseThrow(NoContentException::new);
+
         BuildingLike buildingLike = buildingLikeRequestDto.returnBuildingLikeEntity();
         buildingLike.setBuilding(building);
-        buildingLikeRepository.save(buildingLike);
-        return buildingLike.getId();
+
+        return buildingLikeRepository.save(buildingLike)
+            .getId();
     }
 
     // READ
@@ -46,18 +50,14 @@ public class BuildingLikeService {
     // READ
     @Transactional(readOnly = true)
     public BuildingLikeResponseDto getById(Long id) {
-        BuildingLike buildingLike = buildingLikeRepository
-                .findById(id)
-                .orElseThrow(NoContentException::new);
+        BuildingLike buildingLike = getBuildingLikeById(id);
         return BuildingLikeResponseDto.of(buildingLike);
     }
 
     // UPDATE
     @Transactional
     public void update(Long id, BuildingLikeRequestDto buildingLikeRequestDto) {
-        BuildingLike buildingLike = buildingLikeRepository
-                .findById(id)
-                .orElseThrow(NoContentException::new);
+        BuildingLike buildingLike = getBuildingLikeById(id);
         buildingLike.update(buildingLikeRequestDto);
         buildingLikeRepository.save(buildingLike);
     }
@@ -66,5 +66,11 @@ public class BuildingLikeService {
     @Transactional
     public void deleteById(Long id) {
         buildingLikeRepository.deleteById(id);
+    }
+
+    private BuildingLike getBuildingLikeById(Long id) {
+        return buildingLikeRepository
+            .findById(id)
+            .orElseThrow(NoContentException::new);
     }
 }
