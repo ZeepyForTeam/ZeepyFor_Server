8a8a8d5e43a963cca91f99fe33a946ed5c21a94f
hyunjong_96 <ahajongs@gmail.com>
2021-07-20 01:31:45 +0900

    refactor : auth Controller logout method modify to delete
    -
    
    resolve : #13

--- src/main/java/com/zeepy/server/auth/controller/AuthController.java
+++ src/main/java/com/zeepy/server/auth/controller/AuthController.java
@@ -2,7 +2,7 @@ package com.zeepy.server.auth.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +27,7 @@ public class AuthController {
 		return ResponseEntity.ok().body(tokenResDto);
 	}
 
-	@GetMapping("/logout")
+	@DeleteMapping("/logout")
 	public ResponseEntity<Void> logout(@AuthenticationPrincipal String userEmail) {
 		authService.logout(userEmail);
 		return ResponseEntity.ok().build();
