7a2839bdadcbf6044609d88fda6244df7201d7cb
koreaaerospaceU <zx811@naver.com>
2021-07-25 22:02:31 +0900

    feat: email 관리 기능 제작 - email request dto 제작 - email servcie 제작 - emial controller 제작 - email create 제작 - email 전송 유틸 클래스 제작 - email 도메인 제작

---
+++ src/main/java/com/zeepy/server/email/util/EmailSendUtility.java
@@ -0,0 +1,32 @@
+package com.zeepy.server.email.util;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Minky on 2021-07-25
+ */
+
+@Component
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+public class EmailSendUtility {
+    private final JavaMailSender javaMailSender;
+    private final String fromEmail = "no_reply@zeepy.com";
+
+    public void mailSend(
+            String email,
+            String title,
+            String description
+    ) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setFrom(fromEmail);
+        message.setSubject(title);
+        message.setText(description);
+
+        javaMailSender.send(message);
+    }
+}
