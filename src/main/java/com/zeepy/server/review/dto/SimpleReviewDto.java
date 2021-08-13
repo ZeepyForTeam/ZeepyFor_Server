f629e01d4d9be06b11dc0c8975072f2f9850349a
hyunjong_96 <ahajongs@gmail.com>
2021-08-13 16:01:44 +0900

    feat : simpleReviewDto에 address, reviewDate추가
    -
    
    resolve :

--- src/main/java/com/zeepy/server/review/dto/SimpleReviewDto.java
+++ src/main/java/com/zeepy/server/review/dto/SimpleReviewDto.java
@@ -1,5 +1,7 @@
 package com.zeepy.server.review.dto;
 
+import java.time.LocalDateTime;
+
 import com.zeepy.server.review.domain.CommuncationTendency;
 import com.zeepy.server.review.domain.LessorAge;
 import com.zeepy.server.review.domain.MultiChoiceReview;
@@ -12,20 +14,24 @@ import lombok.NoArgsConstructor;
 @Getter
 public class SimpleReviewDto {
 	private Long id;
+	private String address;
 	private LessorAge lessorAge;
 	private CommuncationTendency communcationTendency;
 	private MultiChoiceReview soundInsulation;
 	private MultiChoiceReview pest;
 	private MultiChoiceReview lightning;
 	private MultiChoiceReview waterPressure;
+	private LocalDateTime reviewDate;
 
 	public SimpleReviewDto(Review review) {
 		this.id = review.getId();
+		this.address = review.getAddress();
 		this.lessorAge = review.getLessorAge();
 		this.communcationTendency = review.getCommunicationTendency();
 		this.soundInsulation = review.getSoundInsulation();
 		this.pest = review.getPest();
 		this.lightning = review.getLightning();
 		this.waterPressure = review.getWaterPressure();
+		this.reviewDate = review.getCreatedDate();
 	}
 }
