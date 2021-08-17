082a92bf8a72b8468efa2f30e12a6c17cc5a62a3
koreaaerospaceU <zx811@naver.com>
2021-08-17 15:24:46 +0900

    feat: 빌딩 종류 Enum class 정의 - 빌딩 종류 Enum class 정의

---
+++ src/main/java/com/zeepy/server/building/domain/BuildingType.java
@@ -0,0 +1,14 @@
+package com.zeepy.server.building.domain;
+
+/**
+ * Created by Minky on 2021-08-17
+ */
+
+/**
+ * 건물 종류 Enum Type 정의
+ */
+public enum BuildingType {
+    OFFICETEL, // 오피스텔
+    ROWHOUSE, // 연립다세대 주택
+    UNKNOWN; // 알 수 없는 건물 종류
+}
