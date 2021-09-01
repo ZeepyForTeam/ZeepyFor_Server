98e465a0b0edce2267b3ac4c1d4ea950bfa77dcc
KimGyeong <rundeshot@kau.kr>
2021-09-01 23:40:09 +0900

    feat: S3 이미지 업로드 API 구현

---
+++ src/main/java/com/zeepy/server/s3/dto/ImageUrlResDto.java
@@ -0,0 +1,12 @@
+package com.zeepy.server.s3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ImageUrlResDto {
+	private String imageUrlResDto;
+}
